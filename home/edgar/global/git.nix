5419e0f288a930569eab4e7fb119c96ece11ee90
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-10-18 18:37:10 +0200

    git: update config

--- home/edgar/global/git.nix
+++ home/edgar/global/git.nix
@@ -8,6 +8,11 @@
     extraConfig = {
       feature.manyFiles = true;
       init.defaultBranch = "main";
+
+      # preserve original committer when rebasing. See <https://stackoverflow.com/a/76325489/10796945>
+      rebase = {
+        instructionFormat = "%s%nexec GIT_COMMITTER_DATE=\"%cI\" GIT_COMMITTER_NAME=\"%cN\" GIT_COMMITTER_EMAIL=\"%cE\" git commit --amend --no-edit --allow-empty --allow-empty-message%n";
+      };
     };
     ignores = [".direnv" "result"];
 
